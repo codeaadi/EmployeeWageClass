30fb770e26a126882252b8a2a3e45d64b79da4fd
codeaadi <adityadubey362@gmail.com>
2023-01-25 09:31:00 +0530

    Added: Welcome Message for employee wage program

---
+++ com/bridgelabz/employeewageclassmethod/EmployeeClass.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeewageclassmethod;
+
+public class EmployeeClass {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Class Method");
+    }
+
+}
